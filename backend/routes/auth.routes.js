33c93f2ad355f6a58e2871c54fe28fcd7824a4f2
Isha <narangisha07@gmail.com>
2024-02-21 19:27:13 +0530

    first commit

---
+++ backend/routes/auth.routes.js
@@ -0,0 +1,6 @@
+import express from "express";
+const router=express.Router();
+router.get("/api/auth/signup",signup);
+router.get("/api/auth/login",login);
+router.get("/api/auth/logout",logout);
+export default router;
